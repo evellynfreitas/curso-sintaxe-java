97fe4ae765960d68c33beea408b48c550452e412
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-03-07 22:33:48 -0300

    Create TipoDeVariaveis.java

---
+++ TipoDeVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TiposDeVariaveis {
+
+	public static void main(String[] args) {
+		// Tipos Primitivos:
+		char letra = 'a';
+		boolean valido = true;
+		byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333;
+		long cpf = 17645978500L;
+		float altura = 1.64F;
+		double salario = 1234.56;
+		
+		//Conversão:
+		short cepShort = (short) cep;
+		
+		//Constantes
+		final double PI = 3.14;
+		final int ANO = 2023;
+	}
+
+}
