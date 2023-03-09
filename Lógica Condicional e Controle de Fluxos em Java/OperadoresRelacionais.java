f9ec4417ca5ef7d2c283c861146f65264cc214c9
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-03-09 12:29:06 -0300

    Rename OperadoresRelacionais.java to Lógica Condicional e Controle de Fluxos em Java/OperadoresRelacionais.java

--- Lógica Condicional e Controle de Fluxos em Java/OperadoresRelacionais.java
+++ Lógica Condicional e Controle de Fluxos em Java/OperadoresRelacionais.java
@@ -6,7 +6,7 @@ public class OperadoresRelacionais {
     	/*Inteiros*/
     	int i1 = 10, i2= 20;
     	
-    	System.out.println("Comparação entre inteiros:");
+    	System.out.println("ComparaÃ§Ã£o entre inteiros:");
     	System.out.println(i1 == i2);
     	System.out.println(i1 != i2);
     	System.out.println(i1 > i2);
@@ -16,7 +16,7 @@ public class OperadoresRelacionais {
     	/*Float*/
     	float f1 = 4.5F, f2 = 3.5F;
     	
-    	System.out.println("\nComparação entre floats:");
+    	System.out.println("\nComparaÃ§Ã£o entre floats:");
     	System.out.println(f1 == f2);
     	System.out.println(f1 != f2);
     	System.out.println(f1 > f2);
@@ -26,54 +26,54 @@ public class OperadoresRelacionais {
     	/*Char*/
     	char c1 = 'a', c2='y';
     	
-    	System.out.println("\nComparação entre char:");
+    	System.out.println("\nComparaÃ§Ã£o entre char:");
     	System.out.println(c1 == c2);
     	System.out.println(c1 != c2);
     	System.out.println(c1 > c2);
     	System.out.println(c1 < c2);
     	
-    	//Um char ser maior que o outro significa que ele possui uma representação binária maior
+    	//Um char ser maior que o outro significa que ele possui uma representaÃ§Ã£o binÃ¡ria maior
     	
     	
     	/*String*/
     	String s1 = "Fulano", s2 = "Fulano";
     	
-    	System.out.println("\nComparação entre Strings:");
+    	System.out.println("\nComparaÃ§Ã£o entre Strings:");
     	System.out.println(s1 == s2);
     	System.out.println(s1 != s2);
 
-    	//Comparações de maior e menor utilizando Strings resultam em erro!!
+    	//ComparaÃ§Ãµes de maior e menor utilizando Strings resultam em erro!!
     	
     	
     	/*Boolean*/
     	boolean b1 = true, b2 = false;
-    	System.out.println("\nComparação entre Booleans:");
+    	System.out.println("\nComparaÃ§Ã£o entre Booleans:");
     	System.out.println(b1 == b2);
     	System.out.println(b1 != b2);
     	
-    	//Comparações de maior e menor utilizando Booleans resultam em erro!!
+    	//ComparaÃ§Ãµes de maior e menor utilizando Booleans resultam em erro!!
     	
     	
     	/*Comparando tipos diferentes*/
     	
-    	System.out.println("\nComparação entre float e int:");
+    	System.out.println("\nComparaÃ§Ã£o entre float e int:");
     	System.out.println(f1 == i1);
     	System.out.println(f1 != i1);
     	System.out.println(f1 > i1);
     	System.out.println(f1 < i1);
     	
-    	System.out.println("\nComparação entre char e int:");
+    	System.out.println("\nComparaÃ§Ã£o entre char e int:");
     	System.out.println(c1 == i1);
     	System.out.println(c1 != i1);
     	System.out.println(c1 > i1);
     	System.out.println(c1 < i1);
     	
-    	System.out.println("\nComparação entre char e float:");
+    	System.out.println("\nComparaÃ§Ã£o entre char e float:");
     	System.out.println(c1 == f1);
     	System.out.println(c1 != f1);
     	System.out.println(c1 > f1);
     	System.out.println(c1 < f1);
     	
-    	//Comparações utilizando os outro tipos de dado resultaram em erro de lógica
+    	//ComparaÃ§Ãµes utilizando os outro tipos de dado resultaram em erro de lÃ³gica
     }
 }
