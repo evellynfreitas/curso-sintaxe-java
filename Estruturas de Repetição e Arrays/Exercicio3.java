fbc1a96978faba85c2853bbf81a740eb07c4273d
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-04-06 15:55:42 -0300

    Create Exercicio3.java

---
+++ Estruturas de Repetição e Arrays/Exercicio3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int qtd=0;
+		int maior=0;
+		int media=0;
+		
+		do {
+			System.out.print("Digite um número: ");
+			int num = scanner.nextInt();
+			qtd++;
+			
+			media+=num;
+			
+			if(qtd==1 || num > maior) maior = num;
+			
+		}while(qtd<5);
+		
+		media /= 5;
+		
+		System.out.println("Maior: "+maior);
+		System.out.println("Media: "+media);
+		scanner.close();
+	}
+
+}
