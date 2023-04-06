fa5e2b3289107b8cd1ac7379ae4d2676fd52f555
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-04-06 15:39:25 -0300

    Add files via upload

---
+++ Estruturas de Repetição e Arrays/Ex1_NomeIdade.java
@@ -0,0 +1,31 @@
+package While;
+import java.util.Scanner;
+
+public class Ex1_NomeIdade {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String nome;
+		int idade;
+		
+		while(true) {
+			System.out.println("Digite o nome: ");
+			nome = scanner.next();
+			
+			if(nome.equals("0")) 
+				break;
+			
+			System.out.println("Digite a idade");
+			idade = scanner.nextInt();
+			
+			System.out.println(nome+" tem "+idade+" anos");
+		}
+		
+		System.out.println("Fim");
+
+		scanner.close();
+
+	}
+
+}
